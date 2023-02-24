0f1dfeb5bd4f213e21dccf3300f619bdffc7b287
Andrew Wei <andrew@sybl.io>
2023-02-24 01:02:12 -0800

    Remove deprecated responseJSON code

--- Sources/NetKit/NetworkTransport.Request.swift
+++ Sources/NetKit/NetworkTransport.Request.swift
@@ -77,78 +77,6 @@ extension NetworkTransport {
   }
 
   /// Sends an async request to the `NetworkEndpoint` provided and parses the response as a `Result`
-  /// with a success value of a JSON object.
-  ///
-  /// - Parameters:
-  ///   - endpoint: The `NetworkEndpoint`.
-  ///   - queue: The dispatch queue used for placing the request.
-  ///   - tag: Custom tag for identifying this request. One will be generated automatically if
-  ///          unspecified.
-  ///   - overwriteExisting: Indicates if this request should overwrite an existing request with the
-  ///                        same tag. If so, the existing request will be cancelled and this new
-  ///                        request will be placed. If `false` and an existing request is active, a
-  ///                        new request will not be placed and the existing active request will be
-  ///                        returned immediately instead.
-  ///   - cancelQuietly: Indicates if this request should cancel quietly without returning an error.
-  ///                    If `false`, cancellations will be treated as an error
-  ///                    (`NetworkError.cancelled`).
-  ///   - completion: Handler invoked when the request completes and a response is received. This
-  ///                 handler transforms the raw response into a `Result` with a JSON decodable
-  ///                 object as its success value and a `NetworkError` as its failure value. More
-  ///                 fine-grained parsing using the response status code is controlled by the
-  ///                 active `NetworkTransportPolicy`, via its member
-  ///                 `parseResponse(_:statusCode:)`.
-  ///
-  /// - Returns: The `Request` object.
-  @discardableResult public func request(
-    _ endpoint: NetworkEndpoint,
-    queue: DispatchQueue = .global(qos: .utility),
-    tag: String? = nil,
-    overwriteExisting: Bool = true,
-    cancelQuietly: Bool = true,
-    completion: @escaping (Result<Any, Error>) -> Void = { _ in }
-  ) -> Request {
-    let tag = tag ?? generateTagFromEndpoint(endpoint)
-
-    if !overwriteExisting, let existingRequest = getActiveRequest(tag: tag) { return existingRequest }
-
-    removeRequestFromQueue(tag: tag)
-
-    log(.debug, mode: logMode) { "Sending \(endpoint.method.rawValue.uppercased()) request with tag <\(tag)> to endpoint \"\(endpoint)\"..." }
-
-    let request = AF.request(
-      endpoint,
-      method: endpoint.method,
-      parameters: getSanitizedParameters(for: endpoint),
-      encoding: getParameterEncoder(for: endpoint),
-      headers: .init(endpoint.headers),
-      interceptor: policy,
-      requestModifier: { urlRequest in
-        urlRequest.timeoutInterval = endpoint.timeout
-      })
-      .validate({ urlRequest, response, data in self.policy.validate(response: response) })
-      .responseJSON(queue: queue) { [weak self] in
-        guard let weakSelf = self else { return }
-
-        let response = weakSelf.parseResponse($0, for: endpoint, tag: tag)
-
-        if
-          cancelQuietly,
-          case .failure(let error) = response,
-          let error = error as? NetworkError,
-          case .cancelled = error
-        {
-          log(.debug, mode: weakSelf.logMode) { "Sending \(endpoint.method.rawValue.uppercased()) request with tag <\(tag)> to endpoint \"\(endpoint)\"... SKIP: Cancelled quietly" }
-          return
-        }
-
-        completion(response)
-      }
-
-    return addRequestToQueue(request: request, tag: tag)
-  }
-
-  /// Sends an async request to the `NetworkEndpoint` provided and parses the response as a `Result`
   /// with no success value (i.e. when the payload is discardable or when the status code is
   /// expected to be `204`).
   ///
@@ -213,7 +141,6 @@ extension NetworkTransport {
           log(.debug, mode: weakSelf.logMode) { "Sending \(endpoint.method.rawValue.uppercased()) request with tag <\(tag)> to endpoint \"\(endpoint)\"... SKIP: Cancelled quietly" }
           return
         }
-
 
         completion(response)
       }
