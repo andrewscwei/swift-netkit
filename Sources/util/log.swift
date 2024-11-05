ab6d011b8ad6ad17d17ddd883d2e4da7e46d3f43
Andrew Wei <andrewscwei@gmail.com>
2024-11-05 12:37:00 +0900

    feat: Update logging

--- Sources/util/log.swift
+++ Sources/util/log.swift
@@ -4,55 +4,38 @@ import Foundation
 struct Log: Sendable {
   enum Mode {
     case none
-    case unified
     case console
   }
 
   let mode: Mode
 
-  func `default`(_ message: String, isPublic: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
-    log(message, level: .default, isPublic: isPublic, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  func `default`(_ message: String) {
+    log(message, level: .default)
   }
 
-  func info(_ message: String, isPublic: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
-    log(message, level: .info, isPublic: isPublic, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  func info(_ message: String) {
+    log(message, level: .info)
   }
 
-  func debug(_ message: String, isPublic: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
-    log(message, level: .debug, isPublic: isPublic, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  func debug(_ message: String) {
+    log(message, level: .debug)
   }
 
-  func error(_ message: String, isPublic: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
-    log(message, level: .error, isPublic: isPublic, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  func error(_ message: String) {
+    log(message, level: .error)
   }
 
-  func fault(_ message: String, isPublic: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
-    log(message, level: .fault, isPublic: isPublic, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  func fault(_ message: String) {
+    log(message, level: .fault)
   }
 
-  private func log(_ message: String, level: OSLogType = .info, isPublic: Bool, fileName: String, functionName: String, lineNumber: Int) {
+  private func log(_ message: String, level: OSLogType = .info) {
     guard mode != .none else { return }
 
 #if !DEBUG
     guard  level != .debug else { return }
 #endif
-
-    if mode == .unified {
-      let fileName = fileName.components(separatedBy: "/").last?.components(separatedBy: ".").first
-      let subsystem = Bundle.main.bundleIdentifier ?? "app"
-      let category = "\(fileName ?? "???"):\(lineNumber)"
-
-      if isPublic {
-        os_log("%{public}@", log: OSLog(subsystem: subsystem, category: category), type: level, message)
-      }
-      else {
-        os_log("%{private}@", log: OSLog(subsystem: subsystem, category: category), type: level, message)
-      }
-    }
-    else {
-      guard level != .default else { return }
-      print(getSymbol(for: level), message)
-    }
+    print("[🌐]", getSymbol(for: level), message)
   }
 
   private func getSymbol(for level: OSLogType) -> String {
@@ -60,7 +43,7 @@ struct Log: Sendable {
     case .fault: return "💀"
     case .error: return "⚠️"
     case .debug: return "👾"
-    case .info: return "🤖"
+    case .info: return "ℹ️"
     default: return ""
     }
   }
